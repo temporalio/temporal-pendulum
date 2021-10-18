da3c9c3eb67be9c73ee7a7c94280384a1e716c29
Joe Bowbeer <joe.bowbeer@gmail.com>
2021-10-18 14:06:15 -0700

    feat: separate definitions from implementations

--- position-node/src/workflows.ts
+++ position-node/src/workflows.ts
@@ -20,23 +20,33 @@ export interface GameInfo {
   speed: number;
 };
 
+export const getGameInfoQuery = defineQuery("getGameInfo");
+
+export const updateGameInfoSignal = defineSignal<[GameInfo]>("updateGameInfo");
+
+export const setupMoveSignal = defineSignal("setupMove");
+
+export const moveSignal = defineSignal("move");
+
+export const exitSignal = defineSignal("exit");
+
 export async function pendulum(info: GameInfo): Promise<void> {
   const exited = new Trigger<void>();
   let gameInfo: GameInfo = info;
 
-  setListener(defineQuery("getGameInfo"), () => gameInfo);
+  setListener(getGameInfoQuery, () => gameInfo);
 
-  setListener(defineSignal<[GameInfo]>("updateGameInfo"), (info: GameInfo) => {
+  setListener(updateGameInfoSignal, (info: GameInfo) => {
     gameInfo = info;
   });
 
-  setListener(defineSignal("setupMove"), () => {
+  setListener(setupMoveSignal, () => {
     gameInfo.angleAccel = (-9.81 / gameInfo.length) * Math.sin(gameInfo.angle);
     gameInfo.angleVelocity += gameInfo.angleAccel * gameInfo.dt;
     gameInfo.angle += gameInfo.angleVelocity * gameInfo.dt;
   });
 
-  setListener(defineSignal("move"), () => {
+  setListener(moveSignal, () => {
     gameInfo.anchorX = gameInfo.width / 2;
     gameInfo.anchorY = gameInfo.height / 4;
     gameInfo.ballX = Math.floor(
@@ -47,7 +57,7 @@ export async function pendulum(info: GameInfo): Promise<void> {
     );
   });
 
-  setListener(defineSignal("exit"), () => {
+  setListener(exitSignal, () => {
     exited.resolve();
   });
 
